2a0c404f1b0ce30fe75f97b0ada57dcb5f51be9a
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-10-10 20:36:02 -0300

    exercicio 3

---
+++ src/exercicio03.ts
@@ -0,0 +1,18 @@
+import { Ponto } from "./types.ts";
+
+interface Poligono{
+    nome:string,
+    editavel:boolean,
+    pontos:Ponto[]
+}
+
+const poligono:Poligono = {
+  nome: "propriedade",
+  editavel: false,
+  pontos: [
+    { x: 1, y: 2 },
+    { x: 3, y: 4 },
+    { x: 2, y: 5 },
+  ],
+};
+console.log("Poligono :", poligono)
